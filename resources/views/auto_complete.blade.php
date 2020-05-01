1ca2dbe0213a0822a593107691187f2344cc37ad
Parasnshah <parasshah438@gmail.com>
2020-05-01 15:15:09 +0530

    modify auto_complete file

--- resources/views/auto_complete.blade.php
+++ resources/views/auto_complete.blade.php
@@ -8,7 +8,8 @@
       <input type="search" class="form-control" id="searchInput" placeholder="Enter a location">
       <!-- Google map -->
       <br>
-        <div id="map"></div>
+        <div class="map" id="map" style="width: 100%; height: 300px;"></div>
+        <br>
         <!-- Display geolocation data -->
         <ul class="geo-data">
             <li>Full Address: <span id="location"></span></li>
